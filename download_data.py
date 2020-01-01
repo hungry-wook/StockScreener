26b4b5322c0abe0b4fca284678bb505c03606697
hungry-wook <doqmffks@naver.com>
2020-01-01 18:54:37 +0900

    주석 수정

--- download_data.py
+++ download_data.py
@@ -17,6 +17,8 @@ df_list.append(kospi_df)
 
 # 종목정보
 krx_df = fdr.StockListing('KRX')
+
+# 각 종목별 주가 데이터
 for symbol in tqdm(krx_df['Symbol']):
     df = fdr.DataReader(symbol, start_date, end_date)
     df['Symbol'] = symbol
@@ -24,5 +26,6 @@ for symbol in tqdm(krx_df['Symbol']):
     df = df.reset_index(drop=True)
     df_list.append(df)
 
+# 하나의 테이블로 병합
 data = pd.concat(df_list, axis=0, sort=False)
 data.to_csv('stock_data.csv', index=False)
