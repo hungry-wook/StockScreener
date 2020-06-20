f9d81c87b728d463098fa47472e9ad4de7972413
hungry-wook <doqmffks@naver.com>
2020-06-21 01:02:05 +0900

    전 종목 및 코스피/코스닥 지수 가격 데이터 가져오는 함수 추가

--- algo_trading/dataset/__init__.py
+++ algo_trading/dataset/__init__.py
@@ -8,6 +8,7 @@ DATASET_DIR = '/'.join(os.path.abspath(__file__).split('/')[:-3])
 DATASET_DIR = os.path.join(DATASET_DIR, 'dataset')
 
 FINANCIAL_SUMMARY_DATA_FILENAME = 'financial_summary_data.pkl'
+STOCK_PRICE_DATA_FILENAME = 'stock_price_data.pkl'
 
 def _make_dir():
     if not os.path.exists(DATASET_DIR):
@@ -39,6 +40,39 @@ def load_financial_summary_data():
                         FINANCIAL_SUMMARY_DATA_FILENAME)
     if not os.path.exists(path):
         download_financial_summary_data()
+
+    with open(path, 'rb') as f:
+        data = pickle.load(f)
+    return data
+
+def download_stock_price_data(start='2001-01-01'):
+
+    """ data: Dict[Text, pd.DataFrame] """
+
+    print('Start Downloading Stock Price Data...')
+    _make_dir()
+
+    dr = DataReader()
+    corp_data = dr.get_corporation_data()
+    data = dict()
+    for code in tqdm(corp_data['종목코드']):
+        df = dr.get_stock_price(code, start)
+        if len(df) > 0:
+            data[code] = df
+    data['KOSPI'] = dr.get_stock_price('KS11', start)
+    data['KOSDAQ'] = dr.get_stock_price('KQ11', start)
+
+    with open(os.path.join(DATASET_DIR,
+                           STOCK_PRICE_DATA_FILENAME), 'wb') as f:
+        pickle.dump(data, f)
+    print('Download Completed!')
+
+def load_stock_price_data():
+
+    path = os.path.join(DATASET_DIR,
+                        STOCK_PRICE_DATA_FILENAME)
+    if not os.path.exists(path):
+        download_stock_price_data()
 
     with open(path, 'rb') as f:
         data = pickle.load(f)
