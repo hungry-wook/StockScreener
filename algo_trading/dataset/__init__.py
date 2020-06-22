572b55251bf3f5761d6101dfef6608e27a8770c0
hungry-wook <doqmffks@naver.com>
2020-06-22 19:57:26 +0900

    증시자금동향 데이터

--- algo_trading/dataset/__init__.py
+++ algo_trading/dataset/__init__.py
@@ -1,5 +1,8 @@
 import os
+import pandas as pd
 import pickle
+import requests
+from bs4 import BeautifulSoup
 from tqdm import tqdm
 from algo_trading.data_reader import DataReader
 
@@ -9,6 +12,7 @@ DATASET_DIR = os.path.join(DATASET_DIR, 'dataset')
 
 FINANCIAL_SUMMARY_DATA_FILENAME = 'financial_summary_data.pkl'
 STOCK_PRICE_DATA_FILENAME = 'stock_price_data.pkl'
+DEPOSIT_DATA_FILENAME = 'deposit_data.pkl'
 
 def _make_dir():
     if not os.path.exists(DATASET_DIR):
@@ -73,6 +77,53 @@ def load_stock_price_data(update=False):
                         STOCK_PRICE_DATA_FILENAME)
     if (not os.path.exists(path)) or update:
         download_stock_price_data()
+
+    with open(path, 'rb') as f:
+        data = pickle.load(f)
+    return data
+
+def download_deposit_data():
+
+    """ data: pd.DataFrame """
+
+    print('Start Downloading Deposit Data...')
+    _make_dir()
+
+    # Get Total Page Number
+    r = requests.get('https://finance.naver.com/sise/sise_deposit.nhn')
+    soup = BeautifulSoup(r.content, 'html.parser')
+    total_page_num = int(soup.find('td', {'class': 'pgRR'})\
+                         .find('a', href=True)['href']\
+                         .split('=')[1])
+
+    # Scrap each page
+    dfs = []
+    for i in tqdm(range(1, total_page_num + 1)):
+        df = pd.read_html('https://finance.naver.com/sise/sise_deposit.nhn?page={}'.format(i),
+                          encoding='euc-kr')[0]
+        dfs.append(df)
+
+    # Merge & Postprocess
+    data = pd.concat(dfs).dropna()
+    data = data.iloc[:,[0,1,3,5,7,9]]
+    data.columns = ['날짜', '고객예탁금', '신용잔고',
+                    '펀드_주식형', '펀드_혼합형', '펀드_채권형']
+    data.index = pd.to_datetime(data['날짜'].apply(lambda x: '20' + x))
+    data.drop('날짜', axis=1, inplace=True)
+
+    with open(os.path.join(DATASET_DIR,
+                           DEPOSIT_DATA_FILENAME), 'wb') as f:
+        pickle.dump(data, f)
+    print('Download Completed!')
+
+def load_deposit_data(update=False):
+
+    """ 증시자금동향 """
+
+    path = os.path.join(DATASET_DIR,
+                        DEPOSIT_DATA_FILENAME)
+    if (not os.path.exists(path)) or update:
+        download_deposit_data()
 
     with open(path, 'rb') as f:
         data = pickle.load(f)
