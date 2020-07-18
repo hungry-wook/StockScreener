51f6926eb1c94bfb9f296de70b658b845da7996b
hungry-wook <doqmffks@naver.com>
2020-07-18 14:46:28 +0900

    예외처리 추가, 버그 수정

--- algo_trading/analytics/metrics.py
+++ algo_trading/analytics/metrics.py
@@ -16,30 +16,56 @@ def rim_price(annual_data: pd.DataFrame, year:int,
 
     Output
         - 적정 주가(float)
+        - RIM 계산에 필요한 데이터가 불충분한 경우에는 exception을 발생시킨다
     """
-    book_value = annual_data.loc['{}/12'.format(year)]['지배주주지분']
+
+    ##### 분석 대상 제외하는 종목 #####
+
+    # 장부가치 체크
+    try:
+        book_value = int(annual_data.loc['{}/12'.format(year)]['지배주주지분'])
+    except:
+        raise ValueError('장부가치 오류')
+
+    # 발행주식수 체크
+    try:
+        _num_stock = int(annual_data.loc['{}/12'.format(year)]['발행주식수'])
+    except:
+        raise ValueError('발행주식수 오류')
+    num_stock = _num_stock * 1000
+
+    # ROE 체크
+    for x in list(annual_data['ROE']):
+        try:
+            float(x)
+        except:
+            raise ValueError('ROE 오류')
+    ##############################
 
     next_year = '{}/12(E)'.format(year+1)
+    # 내년도 컨센서스 존재하는 경우
     if (next_year in annual_data.index) and \
-        (not np.isnan(annual_data.loc[next_year]['ROE'])):
-        # 내년도 컨센서스 존재하는 경우
-        return_on_equity = annual_data.loc[next_year]['ROE']
+        not np.isnan(annual_data.loc[next_year]['ROE']):
+        return_on_equity = float(annual_data.loc[next_year]['ROE'])
+
+    # 내년도 컨센서스가 없는 경우. 직전 N개 년도 데이터 가중평균하여 추정
     else:
-        # 내년도 컨센서스가 없는 경우. 직전 N개년도 데이터 가중평균하여 추정
         return_on_equity = 0
         denominator = 0
         for i in range(0, n_lookback_year):
-            _return_on_equity = annual_data.loc['{}/12'.format(year-i)]['ROE']
+            _return_on_equity = float(annual_data.loc['{}/12'.format(year-i)]['ROE'])
             if np.isnan(_return_on_equity):
                 continue
             else:
                 return_on_equity += (n_lookback_year - i) * _return_on_equity
                 denominator += (i+1)
+        if denominator == 0:
+            # RIM 계산에 필요한 데이터가 불충분한 경우
+            raise ValueError('ROE 추정 불가능')
+
         return_on_equity /= denominator
 
     return_on_equity /= 100
-
-    num_stock = annual_data.loc['{}/12'.format(year)]['발행주식수'] * 1000
 
     excess_return = book_value * (return_on_equity - required_return)
     company_value = book_value + excess_return / required_return
